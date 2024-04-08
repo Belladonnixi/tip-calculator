fcf0a348642e1f6eb132d905b312cf82161ac41c
Jess <jessica_ernst@outlook.de>
2024-04-08 09:17:28 +0200

    Initial Commit

---
+++ tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Jessi on 08.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
