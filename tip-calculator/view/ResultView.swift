72a276660f6643304dc7485adf865cb6f0042764
Jessica Ernst <jessica_ernst@outlook.de>
2024-04-12 13:40:52 +0200

    display result in resultview

--- tip-calculator/view/ResultView.swift
+++ tip-calculator/view/ResultView.swift
@@ -49,11 +49,25 @@ class ResultView: UIView {
         return stackView
     }()
     
+    private let totalTipView: AmountView = {
+        let view = AmountView(
+            title: "Total tip",
+            textAlignement: .right)
+        return view
+    }()
+    
+    private let totalBillView: AmountView = {
+        let view = AmountView(
+            title: "Total bill",
+            textAlignement: .left)
+        return view
+    }()
+    
     private lazy var hStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
-            AmountView(title: "Total bill", textAlignement: .left),
+            totalBillView,
             UIView(),
-            AmountView(title: "Total tip", textAlignement: .right)
+            totalTipView
         ])
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
@@ -67,6 +81,18 @@ class ResultView: UIView {
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(result: Result) {
+        let text = NSMutableAttributedString(
+            string: String(result.amountPerPerson),
+            attributes: [.font: ThemeFont.bold(ofSize: 48)])
+        text.addAttributes(
+            [.font: ThemeFont.bold(ofSize: 24)],
+            range: NSMakeRange(0, 1))
+        amountPerPersonLabel.attributedText = text
+        totalBillView.configure(text: String(result.totalBill))
+        totalTipView.configure(text: String(result.totalTip))
     }
     
     private func layout() {
