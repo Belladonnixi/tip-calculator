326c371815719c4106423b388ada91b64e62bb3b
Jessica Ernst <jessica_ernst@outlook.de>
2024-04-12 10:46:25 +0200

    observe splitView

---
+++ tip-calculator/extension/Int+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Int+Extension.swift
+//  tip-calculator
+//
+//  Created by Jessi on 12.04.24.
+//
+
+import Foundation
+
+extension Int {
+    var stringValue: String {
+        return String(self)
+    }
+}
